a100f4c357a65e3a4e3c86178dcddf42996b77d2
Seb <sebastien.philippe5@gmail.com>
2019-09-25 10:23:46 +0200

    Ajout du renvoi des 10 premières entreprise

--- routes/companies.js
+++ routes/companies.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET /companies */
 router.get('/', function(req, res, next) {
+    const {db} = req.app.locals;
+    db.collection('compagnies').find().skip(0).limit(10).toArray(
+        (err,companies) => res.json(companies));
 
     res.render('index', { title: 'Express' });
 });
